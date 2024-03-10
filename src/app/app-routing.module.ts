38aa753a9b7a8a2d6d1dcd3e3abf606a17a4ed9c
Soundflog <paha-vershinin@mail.ru>
2024-03-10 13:31:04 +0300

    Init packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeTableFormsComponent} from "./pages/home-table-forms/home-table-forms.component";
+import {NavigationFullComponent} from "./pages/navigation-full/navigation-full.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeTableFormsComponent},
+  {path: 'exp', component: NavigationFullComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,93 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+  TuiSvgModule,
+  TuiButtonModule,
+  TuiHostedDropdownModule,
+  TuiDataListModule,
+  TuiExpandModule,
+  TuiSvgDefsHostModule,
+  TuiDropdownModule,
+  TuiTextfieldControllerModule
+} from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {
+  TuiAppearanceModule, TuiAvatarModule, TuiBadgeNotificationModule, TuiButtonGroupModule,
+  TuiCardModule, TuiFadeModule, TuiHeaderModule,
+  TuiIconModule, TuiIconsDirective,
+  TuiIconsModule,
+  TuiNavigationModule, TuiSurfaceModule, TuiTitleModule
+} from '@taiga-ui/experimental'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeTableFormsComponent } from './pages/home-table-forms/home-table-forms.component';
+import {
+  TuiBadgedContentModule,
+  TuiBadgeModule, TuiIslandModule,
+  TuiMarkerIconModule,
+  TuiTabsModule,
+  TuiTilesModule
+} from "@taiga-ui/kit";
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {TuiAppBarModule} from "@taiga-ui/addon-mobile";
+import { NavigationFullComponent } from './pages/navigation-full/navigation-full.component';
+import {TuiDocNavigationModule} from "@taiga-ui/addon-doc";
+import {TuiRepeatTimesModule} from "@taiga-ui/cdk";
+import { FormsHomeComponent } from './components/forms-home/forms-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeTableFormsComponent,
+    NavBarComponent,
+    NavigationFullComponent,
+    FormsHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiTilesModule,
+    TuiSvgModule,
+    TuiAppBarModule,
+    TuiButtonModule,
+    TuiBadgeModule,
+    TuiHostedDropdownModule,
+    TuiDataListModule,
+    TuiAvatarModule,
+    TuiExpandModule,
+    TuiTabsModule,
+    TuiDocNavigationModule,
+    TuiMarkerIconModule,
+    TuiSvgDefsHostModule,
+    TuiBadgeModule,
+    TuiBadgedContentModule,
+    TuiDropdownModule,
+    TuiTextfieldControllerModule,
+    TuiAppearanceModule,
+    TuiIconsModule,
+    TuiNavigationModule,
+    TuiIconModule,
+    TuiCardModule,
+    TuiSurfaceModule,
+    TuiHeaderModule,
+    TuiTitleModule,
+    TuiFadeModule,
+    TuiRepeatTimesModule,
+    TuiBadgeNotificationModule,
+    TuiAvatarModule,
+    TuiButtonGroupModule,
+    TuiIslandModule,
   ],
-  providers: [],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
