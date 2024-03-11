52e98e9bd9d08e71082c54a97c8ef2d2fd0054e7
stud130312 <paha-vershinin@mail.ru>
2024-03-11 23:16:23 +0300

    rename formGroups & tiles with textarea and buttons
    tiles теперь отображаются корректно, gap прописан 10rem

--- src/app/pages/new-form-page/new-form-page.component.ts
+++ src/app/pages/new-form-page/new-form-page.component.ts
@@ -12,13 +12,15 @@ import {IScale} from "../../models/IScale";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewFormPageComponent implements OnInit {
-  input : any;
-  questionFormGroup: FormGroup ;
-  formGroup: FormGroup ;
-  requestedFields: Field[] = [
+
+  questionsFG: FormGroup ;
+  mainFG: FormGroup ;
+  // Неизменяемые поля
+  notEditableFields: Field[] = [
     { name: 'rfield0', type: 'text', label: 'Новая анкета', size: 'l'},
     { name: 'rfield1', type: 'text', label: 'Описание', size: 's'},
   ];
+  // Шкала
   readonly scaleControl = new FormControl();
 
   readonly items = scales;
@@ -28,6 +30,7 @@ export class NewFormPageComponent implements OnInit {
   readonly stringify = (item: IScale): string =>
     `${item.name}`;
 
+  // Изменяемые поля
   fields: Field[] = [
     { name: 'field0', type: 'text', label: 'Введите текст', size: 's'},
     { name: 'field1', type: 'email', label: 'Введите email', size: 's'},
@@ -39,28 +42,28 @@ export class NewFormPageComponent implements OnInit {
     private readonly alerts: TuiAlertService,
     private fb: FormBuilder,
   ){
-    this.questionFormGroup = this.fb.group({});
-    this.formGroup = this.fb.group({});
+    this.questionsFG = this.fb.group({});
+    this.mainFG = this.fb.group({});
   }
 
   ngOnInit(): void {
-    this.questionFormGroup = this.fb.group({
+    this.questionsFG = this.fb.group({
       field0: new FormControl('', Validators.required),
       field1: new FormControl('', Validators.required),
       field2: new FormControl('', Validators.required),
     });
-    this.formGroup = this.fb.group({
+    this.mainFG = this.fb.group({
       rfield0: new FormControl('', Validators.required),
       rfield1: new FormControl('', Validators.required),
       rfield2: new FormControl('', Validators.required),
     })
-    console.log(this.questionFormGroup);
+    console.log(this.questionsFG);
   }
 
   addField() {
     this.alerts.open('Добавлено поле').subscribe();
     this.fields.push({ name: 'field' + this.fields.length, type:  'text', label: 'Введите текст', size: 's'});
-    this.questionFormGroup.addControl(this.fields[this.fields.length - 1].name, new FormControl(''));
+    this.questionsFG.addControl(this.fields[this.fields.length - 1].name, new FormControl(''));
   }
 
   removeField(index: number) {
@@ -75,11 +78,11 @@ export class NewFormPageComponent implements OnInit {
 
   onSubmit() {
 
-    console.log(this.questionFormGroup.controls);
+    console.log(this.questionsFG.controls);
   }
 
   onSubmitForm() {
-    this.formGroup.addControl(this.scaleControl.value, new FormControl(this.scaleControl.value, Validators.required));
-    console.log(this.formGroup.value);
+    this.mainFG.addControl(this.scaleControl.value, new FormControl(this.scaleControl.value, Validators.required));
+    console.log(this.mainFG.value);
   }
 }
