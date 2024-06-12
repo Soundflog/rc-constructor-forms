bce798736af8a13b98eb2a6fece586e8537f4bb5
Soundflog <paha-vershinin@mail.ru>
2024-06-13 00:58:23 +0300

    форма подтверждения на констр. шкалах

--- src/app/components/scale-constructor/scale-constructor.component.ts
+++ src/app/components/scale-constructor/scale-constructor.component.ts
@@ -1,14 +1,14 @@
 import {Component, Inject, Input, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {IInterpretation} from "../../models/IInterpretation";
-import {catchError, Observable, of, startWith, Subject, switchMap, tap} from "rxjs";
-import {IScale} from "../../models/IScale";
-import {TuiAlertService} from "@taiga-ui/core";
+import {tap} from "rxjs";
+import {TuiAlertService, TuiDialogContext, TuiDialogService} from "@taiga-ui/core";
 import {ActivatedRoute, Router} from "@angular/router";
 import {InterpretationService} from "../../services/InterpretationService";
 import {ScaleService} from "../../services/ScaleService";
 import {tuiInputNumberOptionsProvider} from "@taiga-ui/kit";
 import {IScaleInterpretationResponse} from "../../models/ScaleInterpretationResponse";
+import {PolymorpheusContent} from "@tinkoff/ng-polymorpheus";
 
 @Component({
   selector: 'app-scale-constructor',
@@ -27,8 +27,8 @@ export class ScaleConstructorComponent implements OnInit {
   @Input() interpretation: IScaleInterpretationResponse | null;
 
   fields: IInterpretation[] = []
-  urlId : string | null  =  '';
-  maxLengthTextArea =  220;
+  urlId: string | null = '';
+  maxLengthTextArea = 220;
   interpretationFormGroup: FormGroup; // группа интерпретации
 
   constructor(
@@ -39,6 +39,7 @@ export class ScaleConstructorComponent implements OnInit {
     private route: ActivatedRoute,
     private interpretationService: InterpretationService,
     private scaleService: ScaleService,
+    @Inject(TuiDialogService) private readonly dialogs: TuiDialogService
   ) {
     this.interpretationFormGroup = this.fb.group({});
     this.route.paramMap.subscribe(params => {
@@ -59,32 +60,30 @@ export class ScaleConstructorComponent implements OnInit {
       name: [this.interpretation?.name, Validators.required],
       interpretations: this.fb.array([]),
     })
-    this.interpretation?.interpretations.forEach((interpretation: IInterpretation, index) => {
+    this.interpretation?.interpretations.forEach((interpretation: IInterpretation) => {
       const newControl = this.fb.group({
+        id: [interpretation.id],
         description: [interpretation.description, Validators.required],
         minValue: [interpretation.minValue, Validators.required],
-        maxValue: [interpretation.maxValue, Validators.required],
-        scale:  [interpretation.scale],
+        maxValue: [interpretation.maxValue, Validators.required]
       });
       const newField: IInterpretation = {
         id: interpretation.id,
         description: interpretation.description,
         minValue: interpretation.minValue,
-        maxValue: interpretation.maxValue,
-        scale: interpretation.scale,
+        maxValue: interpretation.maxValue
       };
       (this.interpretationFormGroup.get("interpretations") as FormArray).push(newControl);
       this.fields.push(newField)
     })
-    console.log("Fields: ",this.fields);
+    console.log("Fields: ", this.fields);
     console.log("FormGroup: ", this.interpretationFormGroup.value);
   }
 
   // Функция добавления вопроса в форму
   addField(index: number): void {
     this.alerts.open('Добавлено поле').subscribe();
-    const newIndexQuestion = this.fields.length+ 1;
-    const newField:IInterpretation = {
+    const newField: IInterpretation = {
       minValue: 1,
       maxValue: 20,
       description: "",
@@ -93,39 +92,29 @@ export class ScaleConstructorComponent implements OnInit {
     const newControl = this.fb.group({
       description: ["Интерпритация", Validators.required],
       minValue: [1, Validators.required],
-      maxValue: [20, Validators.required],
-      scale: [this.interpretation],
+      maxValue: [20, Validators.required]
     });
     console.log("Add fields: ", this.fields)
     try {
       (this.interpretationFormGroup.get("interpretations") as FormArray).push(newControl);
       console.log("Add control:  ", this.interpretationFormGroup.value);
     } catch (e) {
-      this.alerts.open('Ошибка добавления вопроса', {status:  'error'}).subscribe();
+      this.alerts.open('Ошибка добавления вопроса', {status: 'error'}).subscribe();
     }
   }
 
-  deleteInterpretation(index: number){
-    // const id = this.interpretation?.id;
-    const interpretation  = this.fields[index];
-    if (interpretation.id != null && interpretation.id >= 0) {
-      this.interpretationService.delete(interpretation.id).pipe(
-        tap(()=>{
-          this.fields.splice(index, 1);
-          this.alerts.open('Интерпретация удалена',  {status:'success'}).subscribe();
-        })).subscribe(() => this.routeToList())
-    } else {
-      this.fields.splice(index, 1);
-      this.alerts.open('Произошла ошибка', {status: 'warning'});
-    }
+  deleteInterpretation(index: number) {
+    this.fields.splice(index, 1);
+    (this.interpretationFormGroup.get("interpretations") as FormArray).removeAt(index);
+    this.alerts.open('Удалена интерпретация', {status: 'info'}).subscribe();
   }
 
-  deleteScale(){
-    if (this.interpretation?.id!= null && this.interpretation?.id >= 0)  {
+  deleteScale() {
+    if (this.interpretation?.id != null && this.interpretation?.id >= 0) {
       const id = this.interpretation.id;
       this.scaleService.deleteScale(id).pipe(
-        tap(()=>{
-          this.alerts.open('Шкала удалена',  {status:'success'}).subscribe();
+        tap(() => {
+          this.alerts.open('Шкала удалена', {status: 'success'}).subscribe();
         })).subscribe(() => this.routeToList())
     }
   }
@@ -141,5 +130,9 @@ export class ScaleConstructorComponent implements OnInit {
           .subscribe(() => this.routeToList())
       }
     }
+  }
+
+  showDialog(content: PolymorpheusContent<TuiDialogContext>): void {
+    this.dialogs.open(content).subscribe();
   }
 }
